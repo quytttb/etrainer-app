e3495fa944ebc2b08ebb2f7f375d6cb8f83e0d61
Lê Văn Tuân <tuanlv352@gmail.com>
2025-04-23 11:29:22 +0700

    feat: image description practice

--- app/practice/index.tsx
+++ app/practice/index.tsx
@@ -41,6 +41,74 @@ const Practice = () => {
         onSubmit={(values) => {
           console.log("Submitted values:", values);
         }}
+        questions={[
+          {
+            audio: {
+              name: "BẮC BLING (BẮC NINH) ｜ OFFICIAL MV ｜ HOÀ MINZY ft NS XUÂN HINH x MASEW x TUẤN CRY.m4a",
+              url: "https://res.cloudinary.com/dwyso05qz/video/upload/v1744543083/y8vde5uij3ehkwfuzng3.mp4",
+            },
+            _id: "67fb9d6c4f38816022f6a2f5",
+            type: "IMAGE_DESCRIPTION",
+            question: null,
+            imageUrl:
+              "https://res.cloudinary.com/dwyso05qz/image/upload/v1744543081/c8rj2i64oci0otmhijrn.jpg",
+            answers: [
+              {
+                answer: "A",
+                isCorrect: true,
+                _id: "67fb9d6c4f38816022f6a2f6",
+              },
+              {
+                answer: "B",
+                isCorrect: false,
+                _id: "67fb9d6c4f38816022f6a2f7",
+              },
+              {
+                answer: "C",
+                isCorrect: false,
+                _id: "67fb9d6c4f38816022f6a2f8",
+              },
+            ],
+            questions: null,
+            createdAt: "2025-04-13T11:18:04.064Z",
+            updatedAt: "2025-04-13T11:18:04.064Z",
+            questionNumber: 14,
+            __v: 0,
+          },
+          {
+            audio: {
+              name: "BẮC BLING (BẮC NINH) ｜ OFFICIAL MV ｜ HOÀ MINZY ft NS XUÂN HINH x MASEW x TUẤN CRY.m4a",
+              url: "https://res.cloudinary.com/dwyso05qz/video/upload/v1744541877/thecr0fy6tziant0aky9.mp4",
+            },
+            _id: "67fb98b64c8466b54afc1db6",
+            type: "IMAGE_DESCRIPTION",
+            question: null,
+            imageUrl:
+              "https://res.cloudinary.com/dwyso05qz/image/upload/v1744541877/fn0lyn5urckadwn2va9w.jpg",
+            answers: [
+              {
+                answer: "A",
+                isCorrect: false,
+                _id: "67fb98b64c8466b54afc1db7",
+              },
+              {
+                answer: "B",
+                isCorrect: false,
+                _id: "67fb98b64c8466b54afc1db8",
+              },
+              {
+                answer: "C",
+                isCorrect: true,
+                _id: "67fb98b64c8466b54afc1db9",
+              },
+            ],
+            questions: null,
+            createdAt: "2025-04-13T10:57:58.369Z",
+            updatedAt: "2025-04-13T10:57:58.369Z",
+            questionNumber: 4,
+            __v: 0,
+          },
+        ]}
       />
     );
   }
